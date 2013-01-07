fdfde206025c61190841c6e4f55f13fed94d3a7b
charles <charles@311rh5d01.liaison-intl.com>
2013-01-07 16:07:01 -0500

    Stuff

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ seaport_server.listen(11337)
 
 seaport_server.on('register',function(service) {
 //todo - register the incoming seaport connection as a route!
+  var subdo = "/" + service.role + "*(.:format)"
+  console.log("REG " + subdo)
+  console.log(service)
+  r.match(subdo,"GET").to(service.role + ".get").name(service.role + "_get")
+//  r.match(
 });
 seaport_server.on('free',function(service) {
+  console.log("FREE")
+  r.remove(service.role + "_get")
 //todo - remove the incoming seaport connection from route!
 });
 
@@ -18,18 +25,26 @@ var barista_router_class = require('barista').Router
 var r = new barista_router_class
 
 var http = require('http')
-bouncy(function (req, res, bounce) {
+var http_proxy = require('http-proxy')
+
+http_proxy.createServer(function (req, res, proxy) {
   var b = r.first(req.url)
   var con = b.controller
 
   var ps = seaport_server.query(con)
 
-  if (ps.length === 0) {
+  if (!con || ps.length === 0) {
+    console.log("fallthrough")
     //var res = bounce.respond();
-    res.end('service not available\n');
+    //res.end('service not available\n');
+    proxy.proxyRequest(req,res,{host: "linux3",port: 31337} );
   }
   else {
-    bounce(ps[Math.floor(Math.random() * ps.length)]);
+    console.log("match")
+    console.log(ps)
+    //var p = ps[Math.floor(Math.random() * ps.length)];
+    //proxy.proxyRequest(req,res,
+    proxy.proxyRequest(req,res,ps[Math.floor(Math.random() * ps.length)]);
   }
 
 
@@ -37,4 +52,6 @@ bouncy(function (req, res, bounce) {
 //  res.writeHead(200, {'Content-Type': 'text/plain'})
 //  res.end('Hello World\n')
 }).listen(1337)
+
+
 console.log('Server running at http://311rh5d01:1337/')
